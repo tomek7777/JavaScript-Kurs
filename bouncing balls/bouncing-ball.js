ee670f5826cdad30bccb216a8c9a20daa7a71cee
tomek7777 <89088480+tomek7777@users.noreply.github.com>
2022-12-15 14:58:25 +0100

    bouncing balls

---
+++ bouncing balls/bouncing-ball.js
@@ -0,0 +1,34 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+
+var ball = {
+  x: 50,
+  y: 50,
+  vx: 20,
+  vy: 10,
+  radius: 10,
+  color: 'black',
+  draw: function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+};
+
+function draw() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  ball.draw();
+  ball.x += ball.vx;
+  ball.y += ball.vy;
+  if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+    ball.vy = -ball.vy;
+  }
+  if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+    ball.vx = -ball.vx;
+  }
+}
+
+setInterval(draw, 10);
